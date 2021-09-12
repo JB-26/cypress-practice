60b445b25c33f460ad6b27cdd89834453b126e48
Joshua Blewitt <joshblewitt@protonmail.com>
2021-09-12 18:33:10 +0100

    More tests added!

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 Just practicing Cypress!
+
+The JS file included performs a series of tests across three different viewports:
+- Desktop
+- Tablet
+- Mobile
+
+The names of the viewports are stored in an array, which then uses the forEach method on each size.
+
+The tests look at the Cypress Kitchen Sink (as I want to make sure I'm using a site that allows automated scripts to view it - I don't want to cause any trouble!)
+
+The tests do a variety of actions such as entering text, submitting forms, clearing text and more!
--- cypress/integration/test_site.js
+++ cypress/integration/test_site.js
@@ -1,22 +1,81 @@
+//create array of viewports for test
 const sizes = ["iphone-xr", "ipad-2", "macbook-13"];
 
 describe("Navigate to my site!", () => {
   sizes.forEach((size) => {
-    it("Go to my site!", () => {
+    //using string interpolation to display the name of device
+    it(`Go to kitchen sink and visit "type" section on ${size}`, () => {
       //set viewport
       cy.viewport(size);
       //visit website
       cy.pause();
       cy.visit("");
 
+      //click the link
       cy.pause();
-      cy.contains("type")
-        .click();
+      cy.contains("type").click();
+
+      //get an input and check if its empty
+      cy.get(".action-email").should("be.empty");
 
       //get an input and type into it (then verify)
-      cy.get('.action-email')
-        .type('fake@email.com')
-        .should('have.value', 'fake@email.com')
+      cy.get(".action-email")
+        .type("fake@email.com")
+        .should("have.value", "fake@email.com");
+
+      //check if disabled text area is disabled
+      cy.get(".action-disabled").should("be.disabled");
+    });
+
+    it(`Go to kitchen sink and visit the Traversal section on ${size}`, () => {
+      //set viewport
+      cy.viewport(size);
+
+      //visit website
+      cy.visit("");
+
+      //click the link
+      cy.contains("children").click();
+
+      //get the class and assert
+      cy.get('.traversal-breadcrumb')
+        .children('.active')
+        .should('contain', 'Data');
+      
+      cy.pause();
+      //check the list
+      cy.get('.traversal-list>li')
+        .eq(1).should('contain', 'siamese');
+      //check the list
+      cy.get('.traversal-list>li')
+        .eq(0).should('not.contain', 'persian');
+    });
+
+    it(`Go to kitchen sink and visit the Actions section on ${size}`, () => {
+      //set viewport
+      cy.viewport(size);
+
+      //visit website
+      cy.visit("");
+
+      cy.contains("submit")
+        .click();
+
+      //type in a value
+      cy.get('.action-form')
+        .find('[type="text"]')
+        .type('TEST')
+        .should("have.value", "TEST");
+      
+      //submit the form
+      cy.get('.action-form').submit()
+        .next().should('contain', 'Your form has been submitted!');
+      
+      //clear the text box
+      cy.get('.action-form')
+        .find('[type="text"]')
+        .clear()
+        .should('be.empty')
     });
   });
 });
